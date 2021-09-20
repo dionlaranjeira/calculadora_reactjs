4f3f09f73a5a5677f0c2df6f666a4e5574dbea29
DION RIBEIRO LARANJEIRA <dionribeiro.rr@gmail.com>
2021-09-20 13:34:42 -0400

    Configuração CSS index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import Calculator from './main/Calculator';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <div>
+      <h1>Calculadora</h1>
     <Calculator />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
